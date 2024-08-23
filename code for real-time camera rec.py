f37a032990f15c0df1805e445b016a66d261fc5c
rahul-1803 <116348101+rahul-1803@users.noreply.github.com>
2024-08-23 20:03:49 +0530

    Update code for real-time camera rec.py

--- code for real-time camera rec.py
+++ code for real-time camera rec.py
@@ -1,3 +1,4 @@
+# This code detects real time image through the camera to recognise Kannada text
 import cv2
 import pytesseract
 from langdetect import detect
@@ -6,15 +7,15 @@ from langdetect.lang_detect_exception import LangDetectException
 pytesseract.pytesseract.tesseract_cmd = r"C:\\Program Files\\Tesseract-OCR\\tesseract.exe" # Specify the path to Tesseract executable
 
 def recognize_text_in_frame(frame, langs):
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #then we need to convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Convert the frame to grayscale
 
-    text = pytesseract.image_to_string(gray, lang=langs) #we use pytesseract to do OCR on the frame
+    text = pytesseract.image_to_string(gray, lang=langs) # Perform OCR on the frame
 
     try:
-        language = detect(text) 
-        if language == 'ENGLISH DETECTED':
+        language_code = detect(text)
+        if language_code == 'en':
             language = "English"
-        elif language == 'KANNADA DETECTED':
+        elif language_code == 'kn':
             language = "Kannada"
         else:
             language = "Other"
@@ -24,15 +25,14 @@ def recognize_text_in_frame(frame, langs):
     return text, language
 
 
-cap = cv2.VideoCapture(0) #Initialization of the webcam on laptop
+cap = cv2.VideoCapture(0) # Initialize the webcam
 
 if not cap.isOpened():
     print("Error: Could not open webcam.")
     exit()
 
 while True:
-
-    ret, frame = cap.read()     #the camera captures frame-by-frame
+    ret, frame = cap.read() # Capture frame-by-frame
 
     if not ret:
         print("Error: Could not read frame.")
@@ -47,8 +47,8 @@ while True:
     # Display the resulting frame
     cv2.imshow('Real-time Text Recognition', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'): #press 'q' to exit camera
+    if cv2.waitKey(1) & 0xFF == ord('q'): # Press 'q' to exit camera
         break
 
-cap.release() 
+cap.release()
 cv2.destroyAllWindows()
